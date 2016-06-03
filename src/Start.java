a69b2d2baca325986631b1054f95e271f0e5d2ca
Ashish Singh <ashk43712@gmail.com>
2016-06-03 16:59:07 +0530

    Modify

---
+++ src/Start.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Start{
+	
+	public static void main(String[] args){
+		
+/*		
+		Process P = new Process();
+		try{
+			P.loadData("Data_with_filename.csv");
+		}
+		catch(IOException e){
+			System.out.println("Problem in loading the file");
+		}
+
+		P.cluster(4);
+		
+	}
+*/
+	
+}
