6759308c4b327e7a0bf938c13ec2330ba5759d87
Ashish Singh <ashk43712@gmail.com>
2016-06-03 17:11:36 +0530

    Modify

---
+++ src/Point.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+class Point {
+	protected double[] pos;
+	protected int dimension;
+	public Point(int size){
+		pos = new double[size];
+		this.dimension = size;
+	}
+	public Point(double[] p){
+		this.pos = p;
+		this.dimension = pos.length;
+	}
+	int getDimension(){
+		return this.dimension;
+	}
+	
+	double[] getPosition(){
+		return pos.clone();
+	}
+	
+	public static double euclideanDistance(Point p1, Point p2){
+		if(p1.pos.length != p2.pos.length)
+			return -1.0;
+		double[] p = new double[p1.pos.length];
+		for(int i = 0; i < p1.pos.length; ++i)
+			p[i] = p1.pos[i] - p2.pos[i];
+		double sum = 0.0;
+		for(int i = 0; i < p1.pos.length; ++i){
+			sum += Math.pow(p[i], 2.0);
+		}
+		return Math.sqrt(sum);
+	}
+	
+	public static double squareDistance(Point p1, Point p2){
+		if(p1.pos.length != p2.pos.length)
+			return -1.0;
+		double[] p = new double[p1.pos.length];
+		for(int i = 0; i < p1.pos.length; ++i)
+			p[i] = p1.pos[i] - p2.pos[i];
+		double sum = 0.0;
+		for(int i = 0; i < p1.pos.length; ++i){
+			sum += Math.pow(p[i], 2.0);
+		}
+		return sum;
+	}
+	
+	public boolean equals(Object o){
+		Point p = (Point)o;
+		if(this.dimension != p.dimension)
+			return false;
+		for(int i = 0; i < this.dimension; i++)
+			if(this.pos[i] != p.pos[i])
+				return false;
+		return true;
+	}
+
+}
